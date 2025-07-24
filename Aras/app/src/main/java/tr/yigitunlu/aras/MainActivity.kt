b0bff1848a0c4859722ce1bed62be09a59894818
Yiğit Ünlü <yigitunlu@gmail.com>
2025-07-24 17:08:56 +0300

    hilt and nav

--- Aras/app/src/main/java/tr/yigitunlu/aras/MainActivity.kt
+++ Aras/app/src/main/java/tr/yigitunlu/aras/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import tr.yigitunlu.aras.presentation.TaskListScreen
 import tr.yigitunlu.aras.ui.theme.ArasTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +22,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TaskListScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArasTheme {
-        Greeting("Android")
     }
 }
---
+++ Aras/app/src/main/java/tr/yigitunlu/aras/presentation/TaskListScreen.kt
@@ -0,0 +1,61 @@
+package tr.yigitunlu.aras.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import tr.yigitunlu.aras.domain.model.Task
+
+@Composable
+fun TaskListScreen(
+    viewModel: TaskViewModel = viewModel()
+) {
+    val tasks by viewModel.tasks.collectAsState()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyColumn {
+            items(tasks) { task ->
+                TaskItem(
+                    task = task,
+                    onCheckedChange = {
+                        viewModel.updateTask(task.copy(isCompleted = it))
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskItem(
+    task: Task,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = task.title,
+            modifier = Modifier.weight(1f)
+        )
+        Checkbox(
+            checked = task.isCompleted,
+            onCheckedChange = onCheckedChange
+        )
+    }
+}
