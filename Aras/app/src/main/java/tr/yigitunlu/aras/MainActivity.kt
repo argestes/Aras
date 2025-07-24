4e2059032e34974af0483201c5c70de92295a201
Yiğit Ünlü <yigitunlu@gmail.com>
2025-07-24 17:24:04 +0300

    navigate to addtask

--- Aras/app/src/main/java/tr/yigitunlu/aras/MainActivity.kt
+++ Aras/app/src/main/java/tr/yigitunlu/aras/MainActivity.kt
@@ -5,24 +5,49 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.navigation.BottomSheetNavigator
+import androidx.compose.material.navigation.ModalBottomSheetLayout
+import androidx.compose.material.navigation.bottomSheet
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import tr.yigitunlu.aras.presentation.AddTaskSheet
 import tr.yigitunlu.aras.presentation.TaskListScreen
 import tr.yigitunlu.aras.ui.theme.ArasTheme
 
 @AndroidEntryPoint
+@OptIn(ExperimentalMaterialApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ArasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TaskListScreen()
+                val bottomSheetNavigator = BottomSheetNavigator(
+                    sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+                )
+                val navController = rememberNavController(bottomSheetNavigator)
+
+                ModalBottomSheetLayout(
+                    bottomSheetNavigator = bottomSheetNavigator,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    NavHost(navController, startDestination = "taskList") {
+                        composable("taskList") {
+                            TaskListScreen(navController = navController)
+                        }
+                        bottomSheet("addTask") {
+                            AddTaskSheet {
+                                navController.popBackStack()
+
+                            }
+                        }
+                    }
                 }
             }
         }
--- Aras/app/src/main/java/tr/yigitunlu/aras/presentation/TaskListScreen.kt
+++ Aras/app/src/main/java/tr/yigitunlu/aras/presentation/TaskListScreen.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,24 +20,39 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import tr.yigitunlu.aras.domain.model.Task
 
 @Composable
 fun TaskListScreen(
-    viewModel: TaskViewModel = viewModel()
+    navController: NavController,
+    viewModel: TaskViewModel = hiltViewModel()
 ) {
     val tasks by viewModel.tasks.collectAsState()
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            items(tasks) { task ->
-                TaskItem(
-                    task = task,
-                    onCheckedChange = {
-                        viewModel.updateTask(task.copy(isCompleted = it))
-                    }
-                )
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { navController.navigate("addTask") }) {
+                Icon(Icons.Default.Add, contentDescription = "Add Task")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            LazyColumn {
+                items(tasks) { task ->
+                    TaskItem(
+                        task = task,
+                        onCheckedChange = {
+                            viewModel.updateTask(task.copy(isCompleted = it))
+                        }
+                    )
+                }
             }
         }
     }
